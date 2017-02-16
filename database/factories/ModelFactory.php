bbb34f60883edfe2c9019615cd4ff005c55a8226
NH Evan <nhevan@gmail.com>
2017-02-16 21:31:44 +0600

    Setup a model factory to create tickets.

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Ticket;
+use Picqer\Barcode\BarcodeGeneratorPNG;
+
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -20,5 +23,26 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Ticket::class, function (Faker\Generator $faker) {
+	static $reg_id_start_number = 40000;
+    
+    $last_ticket_id = Ticket::all()->pluck('id')->last();
+    $reg_code = $reg_id_start_number + $last_ticket_id + 1;
+    $reg_id = 'YB-'.$reg_code;
+
+    $generator = new BarcodeGeneratorPNG();
+	$barcode = $generator->getBarcode($reg_id, $generator::TYPE_CODE_128, 3, 170);
+	$barcode = base64_encode($barcode);
+
+    return [
+        'name' => $faker->name,
+        'email' => $faker->unique()->safeEmail,
+        'phone' => $faker->unique()->e164PhoneNumber,
+        'slogan' => $faker->word,
+        'reg_id' => $reg_id,
+        'barcode' => $barcode
     ];
 });
