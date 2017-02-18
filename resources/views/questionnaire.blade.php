dc8a9f60c35b2081d68316e0a32ef7eb2fc61c78
NH Evan <nhevan@gmail.com>
2017-02-18 18:27:22 +0600

    Correct the spelling of questionnaire in blade file.

--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
-                <div class="panel-heading text-center"><h3>Questionaire Page</h3></div>
+                <div class="panel-heading text-center"><h3>Questionnaire Page</h3></div>
                 <div class="panel-body">
                     <form method="POST" action="/register-visitor">
 					    {{ csrf_field() }}
