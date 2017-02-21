53f2f448e9d4d37d52f571914ff9b526e3ccaa31
NH Evan <nhevan@gmail.com>
2017-02-21 21:45:29 +0600

    Implement new basic template and load resources.

--- resources/views/layouts/visitor.blade.php
+++ resources/views/layouts/visitor.blade.php
@@ -22,21 +22,65 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-default navbar-static-top">
-            <div class="container">
-                <div class="navbar-header">
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
+        <div class="container-fluid">
+            <div class="row" style="max-height: 80%">
+                <div class="col-md-4 col-md-offset-1 col-sm-6 col-sm-offset-3">
+                    <img src="{{ URL::asset('images/logo.png') }}" style="width: 100%;margin-bottom: 50px;"/>
+                </div>
+                <div id="yielded_content" class="col-md-6 col-sm-12" style="">
+                    @yield('content')
                 </div>
             </div>
-        </nav>
-
-        @yield('content')
+            <div id="footer" class="row">
+                <div class="col-md-12" style="position: relative;padding: 0;">
+                    <img src="{{ URL::asset('images/footer_image.png') }}" style="width: 100%;"/>
+                    <a href="http://flowdigitalmedia.com"><img src="{{ URL::asset('images/flow_logo.png') }}" style="position: absolute;right: 17px;bottom: 8px;"/></a>
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
+    <style>
+        #app > .container-fluid{
+            padding-top: 30px;
+            min-height: 100vh;
+            position: relative;
+        }
+        #footer{
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            left: 0;
+        }
+        .custom-btn {
+            background: #000;
+            border: 0;
+            padding: 8px;
+            color: #fff;
+            width: 100%;
+            border-radius: 0px;
+        }
+        .my_input {
+            width: 100%;
+            border-top: 0;
+            border-left: 0;
+            border-right: 0;
+            margin-bottom: 8px;
+        }
+        input:focus {
+            outline: none !important;
+        }
+        .bordered{
+            border: 1px solid red;
+            min-height: 20px;
+        }
+        @media (max-width: 992px) {
+            #yielded_content{
+                margin-bottom: 35%;
+            }
+        }
+    </style>
 </body>
 </html>
