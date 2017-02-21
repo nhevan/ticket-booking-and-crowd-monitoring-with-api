41497236a4a9f3762b3d7fe1c93598a64c9aba23
NH Evan <nhevan@gmail.com>
2017-02-21 21:58:50 +0600

    Update questionnaire, registration and success blade files to reflect new visitor template changes.

--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -1,67 +1,60 @@
 @extends('layouts.visitor')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading text-center"><h3>Questionnaire Page</h3></div>
-                <div class="panel-body">
-                    <form method="GET" action="/register-visitor">
-					    {{ csrf_field() }}
-						<p>{{ $question['question'] }}</p>
-						{{ Form::hidden('question', Crypt::encrypt($question['id']) ) }}
-						<div class="radio">
-						  <label>
-						    <input type="radio" name="answer" value="{{ $question['option1'] }}" required>
-						    {{ $question['option1'] }}
-						  </label>
-						</div>
-						<div class="radio">
-						  <label>
-						    <input type="radio" name="answer" value="{{ $question['option2'] }}">
-						    {{ $question['option2'] }}
-						  </label>
-						</div>
-						<div class="radio">
-						  <label>
-						    <input type="radio" name="answer" value="{{ $question['option3'] }}">
-						    {{ $question['option3'] }}
-						  </label>
-						</div>
-						<hr>
-					    <p>৭ই মার্চ আপনার জন্য কি ?</p>
-					    <div class="radio">
-						  <label>
-						    <input type="radio" name="slogan" value="1" required>
-						    আমার সাহস
-						  </label>
-						</div>
-						<div class="radio">
-						  <label>
-						    <input type="radio" name="slogan" value="2">
-						    আমার অহঙ্কার
-						  </label>
-						</div>
-						<div class="radio">
-						  <label>
-						    <input type="radio" name="slogan" value="3">
-						    আমার বিশ্বাস
-						  </label>
-						</div>
-					    <div class="radio">
-						  <label>
-						    <input type="radio" name="slogan" value="4">
-						    আমার প্রেরণা
-						  </label>
-						</div>
-						<div class="form-group">
-					    	<button type="submit" class="btn btn-primary center-block">Submit Answers</button>
-					    </div>
-					</form>
-                </div>
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-body">
+            <form method="GET" action="/register-visitor">
+			    {{ csrf_field() }}
+				<p>{{ $question['question'] }}</p>
+				{{ Form::hidden('question', Crypt::encrypt($question['id']) ) }}
+				<div class="radio">
+				  <label>
+				    <input type="radio" name="answer" value="{{ $question['option1'] }}" required>
+				    {{ $question['option1'] }}
+				  </label>
+				</div>
+				<div class="radio">
+				  <label>
+				    <input type="radio" name="answer" value="{{ $question['option2'] }}">
+				    {{ $question['option2'] }}
+				  </label>
+				</div>
+				<div class="radio">
+				  <label>
+				    <input type="radio" name="answer" value="{{ $question['option3'] }}">
+				    {{ $question['option3'] }}
+				  </label>
+				</div>
+				<hr>
+			    <p>৭ই মার্চ আপনার জন্য কি ?</p>
+			    <div class="radio">
+				  <label>
+				    <input type="radio" name="slogan" value="1" required>
+				    আমার সাহস
+				  </label>
+				</div>
+				<div class="radio">
+				  <label>
+				    <input type="radio" name="slogan" value="2">
+				    আমার অহঙ্কার
+				  </label>
+				</div>
+				<div class="radio">
+				  <label>
+				    <input type="radio" name="slogan" value="3">
+				    আমার বিশ্বাস
+				  </label>
+				</div>
+			    <div class="radio">
+				  <label>
+				    <input type="radio" name="slogan" value="4">
+				    আমার প্রেরণা
+				  </label>
+				</div>
+				<div class="form-group">
+			    	<button type="submit" class="btn btn-primary center-block">Submit Answers</button>
+			    </div>
+			</form>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/register-visitor.blade.php
+++ resources/views/register-visitor.blade.php
@@ -1,43 +1,37 @@
 @extends('layouts.visitor')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-default">
-                <div class="panel-heading text-center"><h3>Enter your details</h3></div>
-                <div class="panel-body">
-                    <form method="POST" action="/generate-ticket">
-					    {{ csrf_field() }}
-					    {{ Form::hidden('slogan', Crypt::encrypt($slogan) ) }}
-					    <div class="form-group">
-					    	<input type="text" class="form-control" name='name' placeholder="Enter your name" value="{{ old('name') }}" required>
-					    </div>
-					    <div class="form-group">
-					    	<input type="email" class="form-control" name='email' placeholder="Enter your email address" value="{{ old('email') }}" required>
-					    </div>
-					    <div class="form-group">
-					    	<div class="input-group">
-					    		<div class="input-group-addon">+88</div>
-						    	<input type="number" class="form-control" name='phone' placeholder="Enter your phone number" value="{{ old('phone') }}" required>
-					    	</div>
-					    </div>
-					    <div class="form-group">
-					    	<button type="submit" class="btn btn-primary center-block">Request Ticket</button>
-					    </div>
-					</form>
-					@if (count($errors))
-						<div class="alert alert-danger">
-							<ul>
-								@foreach ($errors->all() as $error)
-									<li>{{ $error }}</li>
-								@endforeach
-							</ul>
-						</div>
-					@endif
-                </div>
-            </div>
+    <div class="panel panel-default">
+        <div class="panel-heading text-center"><h3>Enter your details</h3></div>
+        <div class="panel-body">
+            <form method="POST" action="/generate-ticket">
+			    {{ csrf_field() }}
+			    {{ Form::hidden('slogan', Crypt::encrypt($slogan) ) }}
+			    <div class="form-group">
+			    	<input type="text" class="form-control" name='name' placeholder="Enter your name" value="{{ old('name') }}" required>
+			    </div>
+			    <div class="form-group">
+			    	<input type="email" class="form-control" name='email' placeholder="Enter your email address" value="{{ old('email') }}" required>
+			    </div>
+			    <div class="form-group">
+			    	<div class="input-group">
+			    		<div class="input-group-addon">+88</div>
+				    	<input type="number" class="form-control" name='phone' placeholder="Enter your phone number" value="{{ old('phone') }}" required>
+			    	</div>
+			    </div>
+			    <div class="form-group">
+			    	<button type="submit" class="btn btn-primary center-block">Request Ticket</button>
+			    </div>
+			</form>
+			@if (count($errors))
+				<div class="alert alert-danger">
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
         </div>
     </div>
-</div>
 @endsection
