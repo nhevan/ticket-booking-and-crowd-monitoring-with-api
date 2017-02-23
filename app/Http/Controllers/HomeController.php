ecec2064b09e72fb4e5c437fb32fe91414121418
NH Evan <nhevan@gmail.com>
2017-02-23 19:58:42 +0600

    Prepare methods to toggle registration process status.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Ticket;
+use App\Setting;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,44 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $tickets = Ticket::paginate(15);
+        // dd($tickets);
+
+        return view('dashboard', compact('tickets'));
+    }
+
+    /**
+     * Swaps the status of registration process from running to pause
+     * @return view [description]
+     */
+    public function swapRegistrationStatus()
+    {
+        $is_registration_allowed = Setting::where('setting', 'is_registration_allowed')->first()->value;
+        if ($is_registration_allowed) {
+            $this->turnOffRegistration();
+        }else{
+            $this->turnOnRegistration();
+        }
+        return back();
+    }
+
+    /**
+     * Turns OFF registration process
+     */
+    public function turnOffRegistration()
+    {
+        $setting = Setting::where('setting', 'is_registration_allowed')->first();
+        $setting->value = 0;
+        $setting->save();
+    }
+
+    /**
+     * Turns ON registration process
+     */
+    public function turnOnRegistration()
+    {
+        $setting = Setting::where('setting', 'is_registration_allowed')->first();
+        $setting->value = 1;
+        $setting->save();
     }
 }
