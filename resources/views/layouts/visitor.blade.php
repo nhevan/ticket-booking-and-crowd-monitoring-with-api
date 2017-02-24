c83010fc3c9b25a361bdf33326f05a7319d00470
Shakil <Shakil>
2017-02-24 10:48:46 +0600

    add jquery from cdn

--- resources/views/layouts/visitor.blade.php
+++ resources/views/layouts/visitor.blade.php
@@ -45,6 +45,7 @@
 
     <!-- Scripts -->
     <script src="/js/app.js"></script>
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
     <script>
         $( function() {
