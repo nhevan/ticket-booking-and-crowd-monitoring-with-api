ce3102b42523baea9508874db2181a3a088af472
Shakil <Shakil>
2017-02-25 22:02:06 +0600

    change text in wrong answer message

--- resources/views/wrong-answer.blade.php
+++ resources/views/wrong-answer.blade.php
@@ -4,7 +4,8 @@
     <div class="panel panel-default" style="margin-top: 100px;">
         <div class="panel-heading text-center"><h3>দুঃখিত !</h3></div>
         <div class="panel-body text-center">
-            <p> আপনার প্রথম প্রশ্নের উত্তরটি ভুল হয়েছে । <a href="/questionnaire">পুনরায় চেষ্টা করুন </a></p>
+            <p> আপনার প্রথম প্রশ্নের উত্তরটি ভুল হয়েছে </p>
+            <p><a href="/questionnaire">আবার চেষ্টা করুন </a></p>
         </div>
     </div>
 @endsection
