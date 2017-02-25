9639010cc42dc3a36f17371c0e5365806c15c5fb
Shakil <Shakil>
2017-02-25 14:16:18 +0600

    change spelling in the wrong answer message

--- resources/views/wrong-answer.blade.php
+++ resources/views/wrong-answer.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
     <div class="panel panel-default" style="margin-top: 100px;">
-        <div class="panel-heading text-center"><h3>দুঃখিত !!!</h3></div>
+        <div class="panel-heading text-center"><h3>দুঃখিত !</h3></div>
         <div class="panel-body text-center">
-            <p> আপনার প্রথম প্রশ্নের উত্তরটি ভুল হয়েছে । <a href="/questionnaire">পুনরায় চেস্টা করুন । </a></p>
+            <p> আপনার প্রথম প্রশ্নের উত্তরটি ভুল হয়েছে । <a href="/questionnaire">পুনরায় চেষ্টা করুন </a></p>
         </div>
     </div>
 @endsection
