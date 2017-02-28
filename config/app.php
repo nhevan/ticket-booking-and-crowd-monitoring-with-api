fc4bc6196bf45b40fb90990756a3f894f4f66df4
NH Evan <nhevan@gmail.com>
2017-02-28 15:14:39 +0600

    Update SMS text message, email message and text on registration on hold page

--- config/app.php
+++ config/app.php
@@ -12,7 +12,7 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'name' => 'Joy Bangla Concert',
+    'name' => 'Joy Bangla Concert 2017',
 
     /*
     |--------------------------------------------------------------------------
--- resources/views/mails/sendTicket.blade.php
+++ resources/views/mails/sendTicket.blade.php
@@ -1,8 +1,9 @@
 @component('mail::message')
 # Ticket Attached
 
-Please print the ticket attached with this email.
+Thank you for registering.
 
+Please print the ticket attached with this email and bring it to the concert. Also carry a photo ID (NID or passport) with you at the concert. In case you can’t make it, you may watch the concert live on Young Bangla and <a href="http://bdnews24.com/">bdnews24.com</a> websites and on Channel 9 and Gaan Bangla.
 
 Thanks,<br>
 {{ config('app.name') }}
