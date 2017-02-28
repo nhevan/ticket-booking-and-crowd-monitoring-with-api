8682c4f8918d2e53990d5932abc0d758838a7f2b
NH Evan <nhevan@gmail.com>
2017-02-28 15:19:50 +0600

    update registration on hold message.

--- resources/views/errors/420.blade.php
+++ resources/views/errors/420.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.visitor')
 
 @section('content')
-	<div class="panel panel-default" style="margin-top: 100px;">
+	<div class="panel panel-default" style="margin-top: 80px;">
         <div class="panel-heading text-center"><h3>Registration On Hold</h3></div>
         <div class="panel-body text-center">
-            <p>Thank you for your interest in Joy Bangla Concert 2017. Unfortunately registration has ended for today. But, we still have plenty of tickets left. So, please come back tomorrow and register.</p>
+            <p>This registration portal will remain open for all from  <span style="color: #ff0000;">3:00pm to 4:00pm</span> every day until Monday, March 6, 2017. If you are seeing this message, it means we have reached the daily limit for registration. Please come back again tomorrow at the above-mentioned time.</p>
         </div>
     </div>
 @endsection
