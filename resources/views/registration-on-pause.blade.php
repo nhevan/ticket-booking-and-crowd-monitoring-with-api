02fe82c01d227026fc753c244cdce560ef761c0f
NH Evan <nhevan@gmail.com>
2017-03-01 18:00:43 +0600

    Update Registration on Hold message t say 3pm-6pm instead of 3pm-4pm

--- resources/views/registration-on-pause.blade.php
+++ resources/views/registration-on-pause.blade.php
@@ -4,7 +4,7 @@
 	<div class="panel panel-default" style="margin-top: 100px;">
         <div class="panel-heading text-center"><h3>Registration On Holdx</h3></div>
         <div class="panel-body text-center">
-            <p>This registration portal will remain open for all from  <span style="color: #ff0000;">3:00pm to 4:00pm</span> every day until Monday, March 6, 2017. If you are seeing this message, it means we have reached the daily limit for registration. Please come back again tomorrow at the above-mentioned time.</p>
+            <p>This registration portal will remain open for all from  <span style="color: #ff0000;">3:00pm to 6:00pm</span> every day until Monday, March 6, 2017. If you are seeing this message, it means we have reached the daily limit for registration. Please come back again tomorrow at the above-mentioned time.</p>
         </div>
     </div>
 @endsection
