cc18eb8192f11ce93d834c612a70100ea019c2fd
NH Evan <nhevan@gmail.com>
2017-03-01 16:34:53 +0600

    Update 500 error page with provided text.

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 	<div class="panel panel-default" style="margin-top: 100px;">
-        <div class="panel-heading text-center"><h3>Something went wrong !!</h3></div>
+        <div class="panel-heading text-center"><h3>Server is Overloaded</h3></div>
         <div class="panel-body text-center">
-            <p>Your request somehow seems to be altered, please try to <a href="/questionnaire">register again</a>.</p>
+            <p>Please try again after a few minutes.<a href="/questionnaire">registration page</a>.</p>
         </div>
     </div>
 @endsection
