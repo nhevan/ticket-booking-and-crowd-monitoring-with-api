3ce08119a82d3d2b4b1adae76a9b6122b7e36086
NH Evan <nhevan@gmail.com>
2017-03-05 12:56:24 +0600

    Update registration on hold message

--- resources/views/registration-on-pause.blade.php
+++ resources/views/registration-on-pause.blade.php
@@ -4,7 +4,7 @@
 	<div class="panel panel-default" style="margin-top: 100px;">
         <div class="panel-heading text-center"><h3>Registration On Hold</h3></div>
         <div class="panel-body text-center">
-            <p>This registration portal will remain open for all from  <span style="color: #ff0000;">7:00am to 5:00pm</span> every day until Monday, March 6, 2017. If you are seeing this message, it means we have reached the daily limit for registration. Please come back again tomorrow at the above-mentioned time.</p>
+            <p>Please come back in the evening to get your free ticket.</p>
         </div>
     </div>
 @endsection
